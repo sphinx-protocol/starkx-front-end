ecf6dcae79322baf86a51c62ff8df369c86285ca
remi gai <remigai@remis-MacBook-Pro.local>
2022-10-24 12:33:50 -0500

    setup base template

---
+++ dex-interface/components/EIP712.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+const EIP712: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Button
+        </button>
+    </div>
+  )
+}
+
+export default EIP712
