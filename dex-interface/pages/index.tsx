60689b4bf02afe9f3c73c4b005ac5693e87455d0
remi gai <remigai@remis-MacBook-Pro.local>
2022-10-24 12:54:53 -0500

    added wagmi

--- dex-interface/pages/index.tsx
+++ dex-interface/pages/index.tsx
@@ -1,32 +1,46 @@
 import type { NextPage } from 'next'
 import EIP712 from "../components/EIP712";
+import Profile from "../components/Profile";
+
 import Head from 'next/head'
 import Image from 'next/image'
 
+import { WagmiConfig, createClient } from 'wagmi'
+import { getDefaultProvider } from 'ethers'
+
+const client = createClient({
+  autoConnect: true,
+  provider: getDefaultProvider(),
+})
+
+
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <WagmiConfig client={client}>
+      <div className="flex min-h-screen flex-col items-center justify-center py-2">
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <EIP712 />
-      </main>
+        <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+          <Profile />
+          <EIP712 />
+        </main>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
+        <footer className="flex h-24 w-full items-center justify-center border-t">
+          <a
+            className="flex items-center justify-center gap-2"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Powered by{' '}
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </a>
+        </footer>
+      </div>
+    </WagmiConfig>
   )
 }
 
