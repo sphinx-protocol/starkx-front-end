77fdbfa7b593eeb93009361b11c31abf26364f71
remi gai <remigai@remis-MacBook-Pro.local>
2022-10-28 12:37:45 -0500

    removed lightchart

--- dex-interface/pages/markets/index.tsx
+++ dex-interface/pages/markets/index.tsx
@@ -5,14 +5,8 @@ import Action from "../../components/Action";
 import TransactionHistory from "../../components/TransactionHistory";
 import OrderBook from "../../components/OrderBook";
 import Orders from "../../components/Orders";
-import { createChart } from 'lightweight-charts';
-
-// const {createChart} = dynamic(import('lightweight-charts'), { ssr: false });
-// import PlotChart from "../../components/PlotChart";
 
 const Markets: NextPage = () => {
-
-
   return (
     <div className="flex flex-col items-center w-full min-h-screen">
     <div className="flex flex-col max-w-7xl border border-black  w-full">
