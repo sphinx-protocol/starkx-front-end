7b191b52fe0c93dc35a72903998a5f2d18d83063
remi gai <remigai@remis-MacBook-Pro.local>
2022-10-28 16:45:05 -0500

    added social media links

--- dex-interface/components/NavBar.tsx
+++ dex-interface/components/NavBar.tsx
@@ -24,11 +24,21 @@ export default function NavBar() {
                     <a href="https://stark-x.gitbook.io/docs/" target="_blank">GitBook</a>
                 </div>
                 <div className="flex flex-row ml-20">
+                <a href="https://twitter.com/starkxdex" target="_blank">
                     <img src={"/twitter.svg"} alt="Twitter Logo" className="w-6 ml-3 cursor-pointer"></img>
+                </a>
+                <a href="https://discord.gg/vgEzd8ddAC" target="_blank">
                     <img src={"/discord.svg"} alt="Discord Logo" className="w-6 ml-3 cursor-pointer"></img>
+                </a>
+                <a href="https://t.me/starkxdex" target="_blank">
                     <img src={"/telegram.svg"} alt="Telegram Logo" className="w-6 ml-3 cursor-pointer"></img>
+                </a>
+                <a href="https://medium.com/@starkxdex" target="_blank">
                     <img src={"/medium.svg"} alt="Medium Logo" className="w-6 ml-3 cursor-pointer"></img>
+                </a>
+                <a href="https://github.com/stark-dex" target="_blank">
                     <img src={"/github.svg"} alt="Github Logo" className="w-6 ml-3 cursor-pointer"></img>
+                </a>
                 </div>
             </div>
             <div className="flex flex-row justify-end w-80">
