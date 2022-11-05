e42eacccf72bd519e39f01510670d2c57ba5745d
remi gai <remigai@remis-MBP.lan>
2022-11-05 12:32:21 -0400

    functional account

--- dex-interface/utils/deployAccount.ts
+++ dex-interface/utils/deployAccount.ts
@@ -24,16 +24,16 @@ const starknetProvider = new Provider({
 });
 
 async function test() {
+  const keypair = ec.getKeyPair("359901924443096931389029071825064737394461357830523568892562288226998109984");
   const starkKeyPair = ec.genKeyPair();
-  const starkKeyPub = ec.getStarkKey(starkKeyPair);
-
-  // const pubkey = ec.getKeyPair("359901924443096931389029071825064737394461357830523568892562288226998109984");
-  // console.log(pubkey);
+  const starkKeyPub = ec.getStarkKey(keypair);
+  // const starkKeyPub = ec.getStarkKey(starkKeyPair);
   const compiledAccount = json.parse(
     fs.readFileSync("./Account.json").toString("ascii")
   );
   const accountResponse = await starknetProvider.deployContract({
     contract: compiledAccount,
+    constructorCalldata: [starkKeyPub],
     addressSalt: starkKeyPub,
   });
   console.log(accountResponse)
