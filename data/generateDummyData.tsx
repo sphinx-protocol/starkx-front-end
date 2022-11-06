5662211d6a6cfd79120a9bc0a2f08452687aed89
remi gai <remigai@remis-MBP.lan>
2022-11-06 13:51:09 -0500

    removed dummy orderbooks

--- data/generateDummyData.tsx
+++ data/generateDummyData.tsx
@@ -1,21 +1,21 @@
-import {BuyOrderBook, SellOrderBook} from "../interfaces/interfaces";
+import {OrderBookProps} from "../interfaces/interfaces";
 
 const randomIntFromInterval = (min: number, max: number) => { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-const generateDummyOrderBooks = (amount :number) => {
-    const buyOrderBook: BuyOrderBook[]  = [];
-    const sellOrderBook: SellOrderBook[] = [];
+// const generateDummyOrderBooks = (amount :number) => {
+//     const buyOrderBook: OrderBookProps[]  = [];
+//     const sellOrderBook: OrderBookProps[] = [];
 
-    for (let i = 0 ; i < amount; i++) {
-        buyOrderBook.push({price: randomIntFromInterval(800, 1500)* 1e18, amount: randomIntFromInterval(1,20)});
-        sellOrderBook.push({price: randomIntFromInterval(1550, 2500)* 1e18, amount: randomIntFromInterval(1,20)});
-    }
+//     for (let i = 0 ; i < amount; i++) {
+//         buyOrderBook.push({price: randomIntFromInterval(800, 1500)* 1e18, amount: randomIntFromInterval(1,20)});
+//         sellOrderBook.push({price: randomIntFromInterval(1550, 2500)* 1e18, amount: randomIntFromInterval(1,20)});
+//     }
 
-    return {buyOrderBook, sellOrderBook};
-}
+//     return {buyOrderBook, sellOrderBook};
+// }
 
-export {
-    generateDummyOrderBooks
-  };
+// export {
+//     generateDummyOrderBooks
+//   };
